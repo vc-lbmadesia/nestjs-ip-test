490f8e2723938bde99e11e6f1956b2d8a7d2d08c
Lb Madesia <lb.madesia@viitor.cloud>
2023-01-04 15:06:33 +0530

    Added IP Test

---
+++ src/app.controller.ts
@@ -0,0 +1,23 @@
+import { Controller, Get, Ip, Req } from '@nestjs/common';
+import { AppService } from './app.service';
+import { HttpService } from '@nestjs/axios';
+import { Request } from 'express';
+import { Observable } from 'rxjs';
+
+@Controller()
+export class AppController {
+  constructor(
+    private readonly appService: AppService,
+    private readonly httpService: HttpService,
+  ) {}
+
+  @Get()
+  async getHello(@Req() request: Request, @Ip() myIp: string): Promise<any> {
+    console.log(myIp);
+    const ipData = await this.httpService
+      .get(`http://ip-api.com/json/24.48.0.1`)
+      .toPromise();
+    return ipData.data;
+    // return this.appService.getHello();
+  }
+}
